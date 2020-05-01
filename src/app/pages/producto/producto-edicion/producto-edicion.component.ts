1c66c9d59adc16a1bc188028b0b87036f8c21d0d
Jaime Reyes <jimmy_melendez@hotmail.com>
2020-05-01 17:36:14 -0600

    funcionalidad de producto

--- src/app/pages/producto/producto-edicion/producto-edicion.component.ts
+++ src/app/pages/producto/producto-edicion/producto-edicion.component.ts
@@ -1,4 +1,9 @@
+import { switchMap } from 'rxjs/operators';
+import { Producto } from './../../../_model/producto';
+import { ProductoService } from './../../../_service/producto.service';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 
 @Component({
   selector: 'app-producto-edicion',
@@ -7,9 +12,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductoEdicionComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  id: number;
+  edicion: boolean;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productoService: ProductoService
+  ) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      'id': new FormControl(0),
+      'nombre': new FormControl(''),
+      'marca': new FormControl('')
+    });
+
+    this.route.params.subscribe((params: Params) => {
+      this.id = params['id'];
+      this.edicion = params['id'] != null;
+      this.initForm();
+    });
+  }
+
+  initForm() {
+    if (this.edicion) {
+      this.productoService.listarPorId(this.id).subscribe(data => {
+        this.form = new FormGroup({
+          'id': new FormControl(data.idProducto),
+          'nombre': new FormControl(data.nombre),
+          'marca': new FormControl(data.marca)
+        });
+      });
+    }
+  }
+
+  operar(){
+    let producto = new Producto();
+    producto.idProducto = this.form.value['id'];
+    producto.nombre = this.form.value['nombre'];
+    producto.marca = this.form.value['marca'];
+
+    if (this.edicion) {
+      this.productoService.modificar(producto).pipe(switchMap( () => {
+        return this.productoService.listar();
+      })).subscribe(data => {
+        this.productoService.productoCambio.next(data);
+        this.productoService.mensajeCambio.next('SE MODIFICO');
+      });
+    } else {
+      this.productoService.resgistrar(producto).pipe(switchMap( () => {
+        return this.productoService.listar();
+      })).subscribe(data => {
+        this.productoService.productoCambio.next(data);
+        this.productoService.mensajeCambio.next('SE REGISTRO');
+      });
+    }
+    this.router.navigate(['producto']);
   }
 
 }
--- src/app/pages/producto/producto.component.ts
+++ src/app/pages/producto/producto.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { switchMap } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ProductoService } from './../../_service/producto.service';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { Producto } from './../../_model/producto';
+import { MatTableDataSource } from '@angular/material/table';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-producto',
@@ -7,9 +14,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductoComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns = ['idProducto', 'nombre', 'marca', 'acciones'];
+  dataSource: MatTableDataSource<Producto>;
+  @ViewChild(MatPaginator)paginator: MatPaginator;
+  @ViewChild(MatSort)sort: MatSort;
+
+  constructor(
+    private productoService: ProductoService,
+    private snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
+    this.productoService.productoCambio.subscribe(data => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+
+    this.productoService.mensajeCambio.subscribe(data => {
+      this.snackBar.open(data, 'AVISO', {
+        duration: 2000
+      });
+    });
+
+    this.productoService.listar().subscribe(data => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort =  this.sort;
+    });
   }
 
+  filtrar(valor: string){
+    this.dataSource.filter = valor.trim().toLowerCase();
+  }
+
+  eliminar(idProducto: number){
+    this.productoService.eliminar(idProducto).pipe(switchMap( () => {
+      return this.productoService.listar();
+    })).subscribe(data => {
+      this.productoService.productoCambio.next(data);
+      this.productoService.mensajeCambio.next('SE ELIMINO');
+    });
+  }
 }
